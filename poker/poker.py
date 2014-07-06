8821f58e9431516ea9a6d00e4f7f56466a9a04fe
Phillip Johnson <phillip@letstalkdata.com>
2014-07-06 18:17:00 -0400

    Tests passing

--- poker/poker.py
+++ poker/poker.py
@@ -1,6 +1,62 @@
 def poker(hands):
-	"Return the best hand: poker([hand,...]) => hand"
-	return max(hands, key=hand_rank)
+    "Return the best hand: poker([hand,...]) => hand"
+    return max(hands, key=hand_rank)
+
+def card_ranks(cards):
+    "Return a list of the ranks, sorted with higher first."
+    ranks = [r for r,s in cards]
+    rankDict = {"A":14,"K":13,"Q":12,"J":11,"T":10}
+    ranks = [int(rankDict.get(r,r)) for r in ranks]
+    ranks.sort(reverse=True)
+    return ranks
+
+def straight(ranks):
+    for i in range(1,len(ranks)):
+        if ranks[i-1] - ranks[i] > 1:
+            return False
+    return True
+
+def flush(hand):
+    suits = [s for r,s in hand]
+    return len(set(suits)) <= 1
+
+def kind(n, ranks):
+    """Return the first rank that this hand has exactly n of.
+    Return None if there is no n-of-a-kind in the hand."""
+    for search_rank in set(ranks):
+        if ranks.count(search_rank)==n:
+            return search_rank
+    return None
+
+def two_pair(ranks):
+    """If there are two pair, return the two ranks as a
+    tuple: (highest, lowest); otherwise return None."""
+    pair1 = kind(2,ranks)
+    pair2 = None
+    if pair1:
+        pair2 = kind(2,[r for r in ranks if r!=pair1])
+    if pair1 and pair2:
+        return (pair1,pair2)
+
+    return None
 
 def hand_rank(hand):
-	return None
+    ranks = card_ranks(hand)
+    if straight(ranks) and flush(hand):            # straight flush
+        return (8, max(ranks))
+    elif kind(4, ranks):                           # 4 of a kind
+        return (7, kind(4, ranks), kind(1, ranks))
+    elif kind(3, ranks) and kind(2, ranks):        # full house
+        return (6,kind(3,ranks),kind(2,ranks))
+    elif flush(hand):                              # flush
+        return (5,ranks)
+    elif straight(ranks):                          # straight
+        return (4,max(ranks))
+    elif kind(3, ranks):                           # 3 of a kind
+        return (3,kind(3,ranks),ranks)
+    elif two_pair(ranks):                          # 2 pair
+        return (2,two_pair(ranks),ranks)
+    elif kind(2, ranks):                           # kind
+        return (1,kind(2,ranks),ranks)
+    else:                                          # high card
+        return (0,ranks)
--- poker/test/test_poker.py
+++ poker/test/test_poker.py
@@ -2,30 +2,87 @@ import unittest
 import poker
 
 class PokerTest(unittest.TestCase):
-	straight_flush = "6C 7C 8C 9C TC".split()
-	four_kind = "9D 9H 9C 9S".split()
-	full_house = "TD TC TH 7C 7D".split()
-	
-	def test_straight_flush_beats_all(self):
-		hands = [self.straight_flush,self.four_kind,self.full_house]
-		self.assertTrue(poker.poker(hands)==self.straight_flush)
+    straight_flush = "6C 7C 8C 9C TC".split()
+    four_kind = "9D 9H 9C 9S 7H".split()
+    full_house = "TD TC TH 7C 7D".split()
 
-	def test_4k_beats_full_house(self):
-		hands = [self.four_kind,self.full_house]
-		self.assertTrue(poker.poker(hands)==self.four_kind)
+    def test_card_ranks(self):
+        self.assertEquals(poker.card_ranks(self.straight_flush),[10,9,8,7,6])
+        self.assertEquals(poker.card_ranks(self.four_kind),[9,9,9,9,7])
+        self.assertEquals(poker.card_ranks(self.full_house),[10,10,10,7,7])
+    
+    def test_straight(self):
+        self.assertTrue(poker.straight([10,9,8,7,6]))
+        self.assertFalse(poker.straight([10,8,7,6,5]))
 
-	def test_full_house_against_self_returns_self(self):
-		hands = [self.full_house,self.full_house]
-		self.assertTrue(poker.poker(hands)==self.full_house)		
+    def test_flush(self):
+        self.assertTrue(poker.flush(self.straight_flush))
+        self.assertFalse(poker.flush(self.four_kind))
 
-	def test_solo_player(self):
-		hands = [self.full_house]
-		self.assertTrue(poker.poker(hands)==self.full_house)
+    def test_kind(self):
+        fkranks = poker.card_ranks(self.four_kind)
+        self.assertEquals(poker.kind(4, fkranks),9)
+        self.assertEquals(poker.kind(3, fkranks),None)
+        self.assertEquals(poker.kind(2, fkranks),None)
+        self.assertEquals(poker.kind(1, fkranks),7)
 
-	def test_100_players(self):
-		hands = [self.full_house] * 99
-		hands + [self.straight_flush]
-		self.assertTrue(poker.poker(hands)==self.straight_flush)	
+    def test_two_pair(self):
+        self.assertEquals(poker.two_pair([10,10,6,6,5]),(10,6))
+        self.assertEquals(poker.two_pair([10,10,6,5,4]),None)
+
+    def test_straight_flush_beats_all(self):
+        hands = [self.straight_flush,self.four_kind,self.full_house]
+        self.assertEquals(poker.poker(hands),self.straight_flush)
+
+    def test_4k_beats_full_house(self):
+        hands = [self.four_kind,self.full_house]
+        self.assertEquals(poker.poker(hands),self.four_kind)
+
+    def test_full_house_against_self_returns_self(self):
+        hands = [self.full_house,self.full_house]
+        self.assertEquals(poker.poker(hands),self.full_house)       
+
+    def test_solo_player(self):
+        hands = [self.full_house]
+        self.assertEquals(poker.poker(hands),self.full_house)
+
+    def test_100_players(self):
+        hands = [self.full_house] * 99
+        hands += [self.straight_flush]
+        self.assertEquals(poker.poker(hands),self.straight_flush)   
+
+    def test_straight_flush_rank(self):
+        self.assertEquals(poker.hand_rank(self.straight_flush),(8,10))
+
+    def test_4k_rank(self):
+        self.assertEquals(poker.hand_rank(self.four_kind),(7,9,7))
+
+    def test_full_house_rank(self):
+        self.assertEquals(poker.hand_rank(self.full_house),(6,10,7))
+
+    def test_flush_rank(self):
+        hand = "KC TC 8C 5C 4C".split()
+        self.assertEquals(poker.hand_rank(hand),(5,[13,10,8,5,4]))
+
+    def test_straight_rank(self):
+        hand = "JC TH 9S 8S 7D".split()
+        self.assertEquals(poker.hand_rank(hand),(4,11))
+
+    def test_3k_rank(self):
+        hand = "5D 5C 5H KS TC".split()
+        self.assertEquals(poker.hand_rank(hand),(3,5,[13,10,5,5,5]))
+
+    def test_two_pair_rank(self):
+        hand = "5D 5C 8H 8S TC".split()
+        self.assertEquals(poker.hand_rank(hand),(2,(8,5),[10,8,8,5,5]))
+
+    def test_pair_rank(self):
+        hand = "5D 3C 8H 8S TC".split()
+        self.assertEquals(poker.hand_rank(hand),(1,8,[10,8,8,5,3]))
+
+    def test_highcard_rank(self):
+        hand = "5D 3C 8H 7S TC".split()
+        self.assertEquals(poker.hand_rank(hand),(0,[10,8,7,5,3]))
 
 if __name__ == '__main__':
     unittest.main()
